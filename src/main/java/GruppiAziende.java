e81f4e1d4fc41cadf13f47ad92a1e2648e50f6ae
ubuntu <you@example.com>
2020-04-30 16:43:42 +0200

    First commit

---
+++ src/main/java/GruppiAziende.java
@@ -0,0 +1,8 @@
+
+public class GruppiAziende {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/main/java/HistoricalStockConstants.java
@@ -0,0 +1,8 @@
+
+public class HistoricalStockConstants {
+	public static final int TICKER = 0;    
+	public static final int EXCHANGE = 1; 
+	public static final int NAME = 2;  
+	public static final int SECTOR = 3;   
+	public static final int INDUSTRY = 4;
+}
